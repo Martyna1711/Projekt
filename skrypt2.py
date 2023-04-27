f59017a03b7469db75af21a6cd3ec6ef78a5c732
Martyna Cegiełka <martyna171114@gmail.com>
2023-04-27 09:36:23 +0200

    zaimportowanie bibliotek i zdefiniowanie głównej funkcji

--- skrypt2.py
+++ skrypt2.py
@@ -4,5 +4,16 @@ Created on Thu Apr 27 08:14:29 2023
 
 @author: Martyna
 """
-def transformacje
+import argparse
+import numpy as np
+from transformacje import Transformacje
 
+
+def main():
+    elipsoida_grs80 = Transformacje(model="grs80")
+
+    parser = argparse.ArgumentParser(description="Program do transformacji współrzędnych.")
+    parser.add_argument("--input", help="ścieżka do pliku wejściowego z danymi")
+    parser.add_argument("--output", help="ścieżka do pliku wyjściowego z wynikami")
+
+    args = parser.parse_args()
